55ed08df3189d2e4b400ddf0ddd5683c9ccd83bd
baonoob1011 <trandinhbao222@gmail.com>
2026-05-31 19:29:13 +0700

    Update InventoryBatchDeductRequest.java

--- src/main/java/com/overcode250204/smartlogicticssystem/dtos/request/InventoryBatchDeductRequest.java
+++ src/main/java/com/overcode250204/smartlogicticssystem/dtos/request/InventoryBatchDeductRequest.java
@@ -5,10 +5,14 @@ import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 public class InventoryBatchDeductRequest {
     @NotNull(message = "Quantity is required")
     @Positive(message = "Quantity must be positive")
     private Integer quantity;
+
+    private LocalDate exportDate;
 }
