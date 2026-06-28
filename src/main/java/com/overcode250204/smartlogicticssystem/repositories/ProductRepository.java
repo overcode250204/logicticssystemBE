f070f78e294e93d4d97d12705b1cce8b553113f6
Trần Đình Bảo <trandinhbao222@gmail.com>
2026-06-28 13:26:32 +0700

    update

--- src/main/java/com/overcode250204/smartlogicticssystem/repositories/ProductRepository.java
+++ src/main/java/com/overcode250204/smartlogicticssystem/repositories/ProductRepository.java
@@ -2,13 +2,13 @@ package com.overcode250204.smartlogicticssystem.repositories;
 
 import com.overcode250204.smartlogicticssystem.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     Optional<Product> findByProductCode(String productCode);
 
     boolean existsByProductCode(String productCode);
